2e1b2e3c041780012d22cbc614caaf684eae94bf
kanbaru-github <usupfv@gmail.com>
2024-12-16 12:13:48 +0900

    Modify var name

--- contests/abc382/c.py
+++ contests/abc382/c.py
@@ -2,13 +2,13 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-r = max(b)
-ans = [-1 for _ in range(r + 1)]
+max_val = max(b)
+ans = [-1 for _ in range(max_val + 1)]
 
 for i in range(n):
-    while a[i] <= r:
-        ans[r] = i + 1
-        r -= 1
+    while a[i] <= max_val:
+        ans[max_val] = i + 1
+        max_val -= 1
 
 for i in range(m):
     print(ans[b[i]])
